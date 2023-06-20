47947d9ccc8c4c32a6668042a3279b81a49721cb
Petr Broz <petr.broz@autodesk.com>
2023-06-20 14:43:14 +0200

    Added experimental GitHub Action.

--- connectors/CustomSharesConnector/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-CONNECTOR="CustomSharesConnector"
-
-mkdir -p bin
-zip bin/${CONNECTOR}.mez ${CONNECTOR}.pq resources.resx ${CONNECTOR}*.png
--- connectors/PropertyDbConnector/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-CONNECTOR="PropertyDbConnector"
-
-mkdir -p bin
-zip bin/${CONNECTOR}.mez ${CONNECTOR}.pq resources.resx ${CONNECTOR}*.png
